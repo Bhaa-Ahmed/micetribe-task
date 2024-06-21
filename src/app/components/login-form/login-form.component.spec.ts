2949f6c959d057a24e1aaac0bf922c11ffc91057
Bahaa <bhaaahmed188@gmail.com>
2024-06-21 21:17:51 +0300

    Add unit test

--- src/app/components/login-form/login-form.component.spec.ts
+++ src/app/components/login-form/login-form.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginFormComponent } from './login-form.component';
+import { By } from '@angular/platform-browser';
+import { ToastrService } from 'ngx-toastr';
 
 describe('LoginFormComponent', () => {
+  const toastrService = {};
+
   let component: LoginFormComponent;
   let fixture: ComponentFixture<LoginFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginFormComponent]
+      imports: [LoginFormComponent],
+      providers: [{ provide: ToastrService, useValue: toastrService }]
     })
     .compileComponents();
     
@@ -20,4 +25,16 @@ describe('LoginFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have undo button as disabled on initialization', () => {
+    const undoBtn = fixture.debugElement.query(By.css('#undo-btn'));
+    fixture.detectChanges();
+    expect(undoBtn.nativeElement.disabled).toBeTrue();
+  })
+
+  it('should have redo button as disabled on initialization', () => {
+    const redoBtn = fixture.debugElement.query(By.css('#redo-btn'));
+    fixture.detectChanges();
+    expect(redoBtn.nativeElement.disabled).toBeTrue();
+  })
 });
--- src/app/services/form.service.spec.ts
+++ src/app/services/form.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FormService } from './form.service';
+import { HistoryArray } from '../enums/enums';
 
 describe('FormService', () => {
   let service: FormService;
@@ -12,5 +13,52 @@ describe('FormService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add a new form value to the undo array', () => {
+    const formValue = JSON.stringify({ firstName: 'John' });
+    service.addNewFormValue(formValue, HistoryArray.UndoArray);
+    expect(service['undoArray'].length).toBe(1);
+  });
+
+  it('should add a new form value to the redo array', () => {
+    const formValue = JSON.stringify({ firstName: 'John' });
+    service.addNewFormValue(formValue, HistoryArray.RedoArray);
+    expect(service['redoArray'].length).toBe(1);
+  });
+
+  it('should not add duplicate form value to the undo array', () => {
+    const formValue = JSON.stringify({ firstName: 'John' });
+    service.addNewFormValue(formValue, HistoryArray.UndoArray);
+    service.addNewFormValue(formValue, HistoryArray.UndoArray);
+    expect(service['undoArray'].length).toBe(1);
+  });
+
+  it('should get the last form value from the undo array and add it to the redo array', () => {
+    const formValue = JSON.stringify({ firstName: 'John' });
+    service.addNewFormValue(formValue, HistoryArray.UndoArray);
+    const returnedFormValue = service.getFormValue(HistoryArray.UndoArray);
+    expect(returnedFormValue).toBeUndefined();
+    expect(service['redoArray'].length).toBe(1);
+  });
+
+  it('should get the last form value from the redo array and add it to the undo array', () => {
+    const formValue = JSON.stringify({ firstName: 'John' });
+    service.addNewFormValue(formValue, HistoryArray.RedoArray);
+    const returnedFormValue = service.getFormValue(HistoryArray.RedoArray);
+    expect(returnedFormValue).toBe(JSON.stringify({ firstName: 'John' }));
+    expect(service['undoArray'].length).toBe(1);
+  });
+
+  it('should update the history state lengths correctly', () => {
+    service['undoArray'].push(JSON.stringify({ firstName: 'John' }));
+    service['redoArray'].push(JSON.stringify({ firstName: 'Doe' }));
+    service.UpdateHistoryStateLength();
+    service.undoArrayLength$.subscribe((length) => {
+      expect(length).toBe(1);
+    });
+    service.redoArrayLength$.subscribe((length) => {
+      expect(length).toBe(1);
+    });
   });
 });
